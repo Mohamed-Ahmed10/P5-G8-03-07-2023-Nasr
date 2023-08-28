8412bce0f60576c23142b709a44adb003d98b0b7
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-08-28 22:21:01 +0300

    Session code

---
+++ js/session.js
@@ -0,0 +1,52 @@
+console.log("I am external")
+
+
+/*
+var x;
+x = 10; // global variable
+
+*/
+
+var x = 10
+console.log(x)
+
+
+var first_num = 50,
+    second_num = 100,
+    total = first_num + second_num;
+
+console.log(total)
+
+var course_name = "JavaScript";
+
+console.log(course_name)
+
+var test;
+console.log(test)
+
+var isSold = true;
+
+console.log(isSold)
+
+console.log(document.getElementById("eghm"))
+
+
+/******************* Concatenation *************************
+var stu_name = "Androw",
+    last_name = "John",
+    full_name = stu_name + " " + last_name;//concatenation
+
+console.log(full_name)
+
+
+var employee_name = prompt("Please enter your first name"),
+    employee_last_name = prompt("Please enter your last name");
+
+console.log("Your full name is "+employee_name + " " + employee_last_name)
+
+*/
+
+// var age = Number(prompt("Please enter your age"));
+var year_of_birth = prompt("Please enter your year of birth");
+
+console.log(2023 - year_of_birth)
