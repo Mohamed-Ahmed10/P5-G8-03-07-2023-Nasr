f590b72ef3e74d889d510e4778dd57db06c7a78d
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-11-06 19:38:00 +0200

    Es6 session

--- js/session.js
+++ js/session.js
@@ -1,29 +1,67 @@
-import add from "./main.js";
-import { multiply } from "./main.js"
+var firstName = "Mohamed",
+    lastName = "Ahmed",
+    btn = document.getElementById("btn")
 
 
+console.log("Hello " + firstName + " " + lastName)
+console.log(`Hello ${firstName} ${lastName}`)
 
-console.log(add(54, 6))
-console.log(multiply(5, 6))
+
+// btn.onclick = function () {
+//     console.log(this) 
+// }
+
+btn.onclick = () => console.log(this)
+
+// console.log(sayHello())
+
+// Spread operator
+
+var students1 = ["test", "test2"],
+    students2 = ["test3", "test4"];
+
+console.log(...students1)
+console.log([...students1, ...students2])
+
+var numbers = [100, 4, 5, 15, 1, 8, 7]
+var max = 0;
+// for (let index = 0; index < numbers.length; index++) {
+
+//     if (numbers[index] > max) { max = numbers[index] }
+
+// }
+console.log(max)
+
+console.log(Math.max(...numbers))
 
 
-var user = {
-    name: "Ali",
-    age: 50,
-    calcAgeInDays: function () {
-        return user.age * 365
-    }
+for (const number of numbers) {
+    console.log(number)
 }
 
-console.log(user.name)
-console.log(user.age)
-console.log(user.calcAgeInDays())
+numbers.forEach((number) => {
+    console.log(number)
+})
 
 
 
-export default class Person {
-    constructor(name = "Unknown person", age = 0) {
-        this.name = name;
-        this.age = age;
-    }
+// Promise
+
+
+const myPromise = new Promise(function (myResolve, myReject) {
+    myResolve("I love You !!")
+});
+myPromise.then(function (value) {
+    console.log(value)
+});
+console.log("test")
+
+
+
+function calc(...numbers) {
+    let sum = 0;
+    for (let arg of numbers) sum += arg;
+    console.log(sum)
 }
+
+calc(5, 7, 8,874)
