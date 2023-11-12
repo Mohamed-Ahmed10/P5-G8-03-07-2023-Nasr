f1601e8ea9834461ade358796ad5758fe614ffca
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-11-12 18:39:23 +0200

    Add

--- js/session.js
+++ js/session.js
@@ -43,3 +43,64 @@ let submission = (x) => {
 var userInput = +prompt("Please enter your number")
 
 console.log(submission(userInput))
+
+
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+*/
+/*
+Examples:
+    (1, 0) --> 1 (1 + 0 = 1)
+    (1, 2) --> 3 (1 + 2 = 3)
+    (0, 1) --> 1 (0 + 1 = 1)
+    (1, 1) --> 1 (1 since both are same)
+    (-1, 0) --> -1 (-1 + 0 = -1)
+    (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+// function getSum(a, b) {
+//     let result = 0;
+//     for (let index = a; index < b; index++) {
+//         result += index;
+//     }
+//     for (let index = a; index > b; index--) {
+//         result += index;
+//     }
+//     if (a !== b) {
+//         result += b;
+//     }
+
+//     return result;
+// }
+
+// OR 
+
+
+
+// OR
+
+function getSum(a, b) {
+    let result = 0;
+  
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+  
+    if(start !== end)
+    {
+        for (let index = start; index <= end; index++) {
+            result += index;
+        }
+    }
+    else
+    {
+      result = "There are tha same"
+    }
+  
+    return result;
+  }
+  
+  console.log(getSum(3, 3));
+  
